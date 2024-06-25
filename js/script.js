232790502ecf5fdae68a4220a55de6a4586360c0
LogikaSchoolUA <115147516+LogikaSchoolUA@users.noreply.github.com>
2024-06-25 05:09:43 +0300

    fix: urls

--- js/script.js
+++ js/script.js
@@ -115,7 +115,7 @@ const cartBtn = document.getElementById('cartBtn');
 // Навішуємо обробник подій на клік кнопки "Кошик"
 cartBtn.addEventListener("click", function () {
     // Переходимо на сторінку кошика
-    window.location.assign('/cart.html');
+    window.location.assign('cart.html');
 });
 
 // Створення класу кошика
